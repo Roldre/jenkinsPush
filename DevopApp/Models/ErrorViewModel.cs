99e0c01e3e6565abcb548abe524fd68fbc0f3251
Nasrullah <naso@ebanqo.com>
2024-04-06 15:10:20 +0100

    init push

---
+++ DevopApp/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace DevopApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
